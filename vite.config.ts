210e2189de1927b0bca92cca0b10dc1da1adce38
danielshapira1 <92857488+danielshapira1@users.noreply.github.com>
2025-08-24 14:18:00 +0300

    added fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 plugins: [react()],
-base: 'https://github.com/danielshapira1/nutriplanner',
+base: 'https://github.com/danielshapira1/nutriplanner/',
 });
